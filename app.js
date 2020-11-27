15f20ecdc260ba8cc6333b1c03a7fddcbd20c38c
somi <somidevelop@gmail.com>
2020-11-28 00:56:36 +0900

    feat: production grade 에서의 설계 실습

--- app.js
+++ app.js
@@ -1,42 +1,32 @@
 var debug = require('debug')('express-tutorial:server');
-require('dotenv').config;
+require('dotenv').config
 
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import createError from 'http-errors'
+import express from 'express'
+import cookieParser from 'cookie-parser'
+import logger from 'morgan'
+import indexRouter from './routes/index'
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express()
 
-var app = express();
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use('/', indexRouter)
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+app.use((req, res, next) => {
+  next(createError(404))
+})
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message
+  res.locals.error = process.env.NODE_ENV === 'development' ? err : {}
+return res.status(err.status || 500)
+  .json(res.locals.error)
+})
 
-  // render the error page
-  res.status(err.status || 500);
-  res.json({
-    message: err.message,
-    error: err
-  });
-});
-
-module.exports = app;
+// bin/www를 그대로 사용하기 위해 예외적으로 commonJs 문법 유지
+module.exports = app
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+import express from 'express'
+import {index} from '../controllers/index.controller'
+
+const router = express.Router()
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.json({message: 'Hello dinos!'});
-});
+router.get('/', index)
 
-module.exports = router;
+export default router;
